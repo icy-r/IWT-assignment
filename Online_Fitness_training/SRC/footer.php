05e768f27ca16fbf04a652a60536a12010c8e5ce
icy <asath12882@gmail.com>
2023-10-05 15:47:37 +0530

    rebased to php

---
+++ Online_Fitness_training/SRC/footer.php
@@ -0,0 +1,41 @@
+
+<footer>
+        <div class="f1rst">
+    <div class="footer1">
+        <a href="#"><img class="footer-logo" src="images/templogo.png" alt="logo"></a>
+        <a class="footer-logo-text" href="#">FLEXIT</a>
+        
+    </div>
+    <div class="social">
+        <a href="#"><img src="images/youtube.png" alt="youtube"></a>
+        <a href="#"><img src="images/twitter.png" alt="twitter"></a>
+        <a href="#"><img src="images/instagram.png" alt="instagram"></a>
+        <a href="#"><img src="images/facebook.png" alt="facebook"></a>
+    </div>
+    <div class="policy">
+        <a class="policy" href="#">Privacy policy | Term of services</a>
+    </div>
+</div>
+    <div class="footer-about-parent">
+        <div class="policy">
+            <p class="footer-about">
+                <span>FLEXIT</span> <br> is a web application that provides a platform for fitness trainers to connect with their clients. <br>
+            </p>
+        </div>
+        <div class="policy1">
+            <p class="footer-text">© 2021 FLEXIT. All rights reserved.</p>
+        </div>
+    </div>
+    <div class="navfoot">
+        <div class="footnav">
+        <p class="contact-details">
+            <span id="contact-details">Phone:</span> 077 123 4567 <br>
+            <span id="contact-details">Email</span> abc@gmail.com <br>
+            <span id="contact-details">Address:</span> 123, Galle Road, <br> Colombo 03, Sri Lanka
+
+        </p>
+        </div>
+        
+    </div>
+    
+    </footer>
---
+++ Online_Fitness_training/SRC/header.php
@@ -0,0 +1,58 @@
+
+<?php
+// your code goes here
+
+
+?>
+
+<!-- FILEPATH: GitHub\IWT-assignment\Online_Fitness_training\SRC\index.html -->
+<!-- 
+This is the HTML code for the index page of the Online Fitness Training website. 
+It includes the header, navigation bar, and footer of the page. 
+The header contains the logo, site name, logout button, and profile icon. 
+The navigation bar contains links to the Home, About, Contact, and Help pages. 
+The footer contains the logo, site name, social media icons, and links to the Privacy Policy and Terms of Service pages. 
+-->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>FLEXIT</title>
+        <link class="title-image" rel="icon" type="image/png" href="images/templogo.png"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link id="stylesheet" rel="stylesheet" href="style/style.css">
+        <script src="script/script.js"></script>
+    </head>
+    <body>
+    
+        <header class="body">
+            <div class="container">
+                    <div>
+                    <a href="#"><img class="logo" src="images/templogo.png" alt="LOGO: fit coach pro"></a>
+                    </div>
+                    <div>
+                    <p class="sitename">
+                        <span id="sitename">FLEXIT</span> <br>
+                        FIT COACH PRO
+                    </p>
+                    </div>
+                    <div><a href="#"><img class="logout" src="images/log-out.png" alt="logout"></a>
+                    <a href="#"><img class="profile-icon" src="images/profile-circle.png" alt="Profile"></a>            
+                    </div>
+                </div>
+            <hr class="line">
+            <div class="navbar">
+            <nav>
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">DASHBOARD</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                    <li><a href="#">HELP</a></li>
+                </ul>
+            </nav>
+            
+            </div>
+            <hr class="line">
+        </header>
+      
